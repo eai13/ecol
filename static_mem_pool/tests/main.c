11d6c8faaa2e60b29fc0350a6e294167fef8f4d5
eai13 <eai84199@gmail.com>
2023-07-01 02:46:19 +0700

    Added smp_realloc

--- static_mem_pool/tests/main.c
+++ static_mem_pool/tests/main.c
@@ -5,69 +5,15 @@
 int main(){
     smp_initialize();
     print_map();
-    printf("void * size : %d\r\n", sizeof(void *));
-    uint8_t * ptr_1 = smp_malloc(8);
-    memset(ptr_1, 0xAA, 8);
-    printf("%d\r\n", get_local(ptr_1));
-    print_map();
-    uint8_t * ptr_2 = smp_malloc(8);
-    memset(ptr_2, 0xBB, 8);
-    printf("%d\r\n", get_local(ptr_2));
-    print_map();
-    uint8_t * ptr_3 = smp_malloc(8);
-    memset(ptr_3, 0xCC, 8);
-    printf("%d\r\n", get_local(ptr_3));
-    print_map();
-    uint8_t * ptr_4 = smp_malloc(8);
-    memset(ptr_4, 0xDD, 8);
-    printf("%d\r\n", get_local(ptr_4));
+    uint8_t * ptr_1 = smp_malloc(16);
+    memset(ptr_1, 0xAA, 16);
     print_map();
 
-    printf("Free status 0x%X\r\n", smp_free(ptr_2));
+    printf("status %d\r\n",ptr_1 == smp_realloc(ptr_1, 4));
     print_map();
 
-    ptr_2 = smp_malloc(8);
-    memset(ptr_2, 0xEE, 8);
-    printf("%d\r\n", get_local(ptr_2));
+    uint8_t * ptr_2 = smp_malloc(6);
+    memset(ptr_2, 0xBB, 6);
     print_map();
-
-    printf("Free status 0x%X\r\n", smp_free(ptr_1));
-    print_map();
-    
-    ptr_1 = smp_malloc(8);
-    memset(ptr_1, 0xFF, 8);
-    printf("%d\r\n", get_local(ptr_1));
-    print_map();
-
-    printf("Free status 0x%X\r\n", smp_free(ptr_1));
-    print_map();
-    printf("Free status 0x%X\r\n", smp_free(ptr_2));
-    print_map();
-    printf("Free status 0x%X\r\n", smp_free(ptr_3));
-    print_map();
-    printf("Free status 0x%X\r\n", smp_free(ptr_4));
-    print_map();
-
-    printf("void * size : %d\r\n", sizeof(void *));
-    ptr_1 = smp_malloc(8);
-    memset(ptr_1, 0xAA, 8);
-    printf("%d\r\n", get_local(ptr_1));
-    print_map();
-    ptr_2 = smp_malloc(8);
-    memset(ptr_2, 0xBB, 8);
-    printf("%d\r\n", get_local(ptr_2));
-    print_map();
-    ptr_3 = smp_malloc(8);
-    memset(ptr_3, 0xCC, 8);
-    printf("%d\r\n", get_local(ptr_3));
-    print_map();
-    ptr_4 = smp_malloc(8);
-    memset(ptr_4, 0xDD, 8);
-    printf("%d\r\n", get_local(ptr_4));
-    print_map();
-
-    
-    // printf("%d\r\n", get_local(smp_malloc(8)));
-    // print_map();
     return 0;
 }
